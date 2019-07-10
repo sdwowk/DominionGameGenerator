60a68bbe8b72b669826408583c50e31e0081a201
Scott Wowk <sdwowk@ualberta.ca>
2019-07-10 00:05:46 -0600

    add functionality to reducer to remove a chosen deck

--- client/src/reducer.js
+++ client/src/reducer.js
@@ -1,61 +1,71 @@
-import { List, Map } from 'immutable';
+import { List, Map } from "immutable";
 
 function setState(state, newState) {
-    return state.merge(newState);
+  return state.merge(newState);
 }
 
 function addToPair(state, entry) {
-    const currentPair = state.get('pair');
-    const removeFromList = state.get('decks');
-    if(currentPair && currentPair.length < 2){
-        currentPair.push(entry);
-        removeFromList.splice(removeFromList.indexOf(entry));
-        console.log("add to pair");
-        console.log(state);
-        return state.merge({
-            'decks': removeFromList,
-            'pair': currentPair
-        });
-
-    }else if(!currentPair){
-        removeFromList.splice(removeFromList.indexOf(entry));
-        console.log("no pair");
-        console.log(state);
-        return state.merge({
-            'decks': removeFromList,
-            'pair': [entry]
-        });
-
-    }
+  const currentPair = state.get("pair");
+  const removeFromList = state.get("decks");
+  if (currentPair && currentPair.length < 2) {
+    currentPair.push(entry);
+    removeFromList.splice(removeFromList.indexOf(entry), 1);
+    console.log("add to pair");
     console.log(state);
-    return state
+    return state.merge({
+      decks: removeFromList,
+      pair: currentPair
+    });
+  } else if (!currentPair) {
+    removeFromList.splice(removeFromList.indexOf(entry), 1);
+    console.log("no pair");
+    console.log(state);
+    return state.merge({
+      decks: removeFromList,
+      pair: [entry]
+    });
+  }
+  console.log(state);
+  return state;
+}
+
+function removeFromPair(state,entry) {
+  const removeFromPair = state.get("pair");
+  removeFromPair.splice(removeFromPair.indexOf(entry), 1);
+  state.merge({
+    pair: removeFromPair,
+    decks: state.get('decks').push(entry)
+  });
+  return state;
 }
 
 function resetVote(state) {
-    const removeFromList = state.get('decks');
-    removeFromList.remove(entry);
-    const currentRound = state.get('pair');
-    if (votedForRound !== currentRound) {
-        return state.remove('myVote');
-    } else {
-        return state;
-    }
+  const removeFromList = state.get("decks");
+  removeFromList.remove(entry);
+  const currentRound = state.get("pair");
+  if (votedForRound !== currentRound) {
+    return state.remove("myVote");
+  } else {
+    return state;
+  }
 }
 
-function getDecks(state){
-    const decks = require("./DominionDecks.json");
-    return state.set('decks', decks);
+function getDecks(state) {
+  const decks = require("./DominionDecks.json");
+  return state.set("decks", decks);
 }
 
-export default function (state = Map(), action) {
-    switch (action.type) {
-        case 'GET_DECKS':
-            return getDecks(state);
-        case 'SET_STATE':
-            return resetVote(setState(state, action.state));
-        case 'ADD_TO_PAIR':
-            return addToPair(state, action.entry);
-        default:
-            return state;
-    }
+export default function(state = Map(), action) {
+  switch (action.type) {
+    case "GET_DECKS":
+      return getDecks(state);
+    case "SET_STATE":
+      return resetVote(setState(state, action.state));
+    case "ADD_TO_PAIR":
+      return addToPair(state, action.entry);
+    case "REMOVE_FROM_PAIR":
+      return removeFromPair(state, action.entry);
+    default:
+      return state;
+  }
 }
